c1138491532b58d479989410f07436c89f8dc2b0
Ver_s <aleksandra.bystrova@alatoo.edu.kg>
2025-10-18 00:37:28 +0600

    INIT: basic qr generating

--- src/QRCodeGenerator.jsx
+++ src/QRCodeGenerator.jsx
@@ -1,11 +1,35 @@
 // здесь будет вся логика, которая генерирует QR Code
 
 import { QRCodeSVG } from "qrcode.react";
+import { useState } from "react";
 
 export const QRCodeGenerator = () => {
+  const [value, setValue] = useState(''); //  useState(''); - по дефолту в инпуте ничего
+  // value - обновляемая переменная, setValue - функция
+
+  const [result, setResult] = useState('');
+
+  const onClickHandler = (e) => {
+    // событие по клику на кнопку
+    setResult(value);
+    setValue("");
+  };
+
+  const onChangeHandler = (e) => {
+    setValue(e.target.value);
+    setResult('');
+  };
+
+  console.log(result);
+
   return (
     <div>
-      <QRCodeSVG value="я тебя люблю :3" />
+      <input type="text" value={value} onChange={onChangeHandler} />
+      <button type="button" onClick={onClickHandler}>
+        Сгенерировать QR
+      </button>
+       {result!== '' && <QRCodeSVG value={result} />}; 
+     
     </div>
   );
 };
